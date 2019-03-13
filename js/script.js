a5fd9b9d2e354f2e6f795c99721af108d23b2f9e
Alex Huynh <alexh3925@gmail.com>
2019-03-13 00:56:10 +0000

    Added random gifs function

--- js/script.js
+++ js/script.js
@@ -4,17 +4,23 @@
 /* global $ */
 
 $("#search-button").click(function(){
+  var gifs = [];
   var input = "https://api.giphy.com/v1/gifs/search?q=" + $("#search-term").val() + "&rating=" + $("#search-age-rating").val() + "&api_key=dc6zaTOxFJmzC&limit=" + $("#search-limit").val();
   $.ajax({
     url: input,
     method: "GET",
     success: function(response) {
       $(".text-center").html("");
-      var gifs = Math.floor(Math.random() * response.data.length);
-      $(".text-center").append("<img class='text-center' src='" + response.data[gifs].images.original.url + "'>");
+      while(gifs.length < $("#search-limit").val()) {
+        var randomNum = Math.floor(Math.random() * response.data.length);
+        if(gifs.includes(randomNum)) {
+          console.log(randomNum);
+        }else {
+          gifs.push(randomNum);
+          $(".text-center").append("<img class='text-center' src='" + response.data[gifs[gifs.indexOf(randomNum)]].images.original.url + "'>");
+        }
+      }
   },
 });
-  
-  
 });
 
